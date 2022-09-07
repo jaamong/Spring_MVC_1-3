7dc97adad17cc3b9ddb89062a3fb6515382468f1
jaamong <amypure225@naver.com>
2022-09-07 16:35:03 +0900

    스프링 MVC - 웹 페이지 만들기 RedirectAttributes

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -100,11 +101,26 @@ public class BasicItemController {
     /**
      * 상품 등록 후 중복 방지를 위해 redirect (PRG)
      */
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(Item item) {
 
         itemRepository.save(item);
-        return "redirect:/basic/items/" + item.getId();
+        return "redirect:/basic/items/" + item.getId(); //url에 변수 더해서 사용 : url 인코딩이 안되서 위험
+    }
+
+    /**
+     * RedirectAttribute
+     *  - url 인코딩 해결
+     *  - pathVariable 바인딩
+     *  - 나머지는 쿼리 파라미터로 처리
+     */
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
