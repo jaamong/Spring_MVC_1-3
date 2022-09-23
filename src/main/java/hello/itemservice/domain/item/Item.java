ea32bdeff9dba8d692277cb25375c7da34e87499
jaamong <amypure225@naver.com>
2022-09-23 22:30:48 +0900

    @Data 설명 정리 Item.java

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,7 +2,9 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 
-//핵심 모델 도메인에서 @Data를 쓰는 건 위험함, Dto에서 쓰는 건 괜춘(but, 확인 필수). 여기서는 쓸거임
+// 핵심 모델 도메인에서 @Data를 쓰는 건 위험
+// Dto에서 쓰는 건 OK (but, 확인 필수).
+// 여기서는 편의상 사용
 @Data
 //@Getter @Setter
 public class Item {
